e755e17c8ca3c417672e63d2032f110e1a4ed0c5
monjurctg <monjurpy.06@gmail.com>
2025-08-30 23:36:19 +0600

    update

--- resources/views/frontend/partials/homepage2.blade.php
+++ resources/views/frontend/partials/homepage2.blade.php
@@ -13,11 +13,11 @@
                     <div class="row justify-content-center text-center">
                         <div class="col-12 col-md-10 col-lg-8">
                             <div class="slider-content text-white">
-                                <h1 class="fw-bold">{{ $row->title }}</h1>
+                                <!-- <h1 class="fw-bold">{{ $row->title }}</h1>
 
                                 @if(!empty($aRow->sub_title))
                                     <p class="mt-3">{{ $aRow->sub_title }}</p>
-                                @endif
+                                @endif -->
 
                                 @if(!empty($aRow->button_text))
                                     <a href="{{ $row->url }}"
